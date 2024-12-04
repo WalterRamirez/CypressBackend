7e852e637ee8d6655640fdbdd2261dd6f7b3b9b6
antonio200x@gmail.com <antonio200x@gmail.com>
2024-12-04 17:13:12 -0400

    Completed First API test

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,10 +1,32 @@
 describe('test with Backend', () => {
-
+  
   beforeEach('Login to Application', () => {
     cy.loginToApplication()
   })
 
-  it('passes', () => {
-    cy.log('Logged In')
+  it.only('Verify request & response', () => {
+
+    // Start the intercept before to submit the info
+    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
+
+    // Submit the info from WebUI
+    cy.contains('New Article').click()
+    cy.get('[formcontrolname="title"]').type('Walter\'s Article Title')
+    cy.get('[formcontrolname="description"]').type('Walter\'s Description')
+    cy.get('[formcontrolname="body"]').type('My\nMultiline\nBody\n\n Bye!')
+    cy.get('[placeholder="Enter tags"]').type('test, tags, backend, QA')
+    cy.contains('Publish Article').click()
+
+    // Wait for the response from the interceptor
+    cy.wait('@postArticles').then(xhr => {
+
+    console.log(xhr)
+    expect(xhr.response.statusCode).to.equal(201)
+    expect(xhr.response.body.article.body).to.equal("My\nMultiline\nBody\n\n Bye!")
+    expect(xhr.response.body.article.description).to.equal("Walter's Description")
+
+    // Deleting the Article in order to clean the Environment
+    cy.get('.banner').contains('Delete Article').click()
+    })
   })
 })
